e4edad79c1d4f9b86204883e7b7127ca802279f6
marcosptf <marcosptf@yahoo.com.br>
2015-03-04 12:35:31 -0300

    Update bootstrap.js

--- public_html/js/bootstrap.js
+++ public_html/js/bootstrap.js
@@ -4,13 +4,17 @@
  * @desc:atribuição dos elementos html nas propriedades do jogo com YUI();
  ********************************************************************************/
 
-window.onload = function() {
+(function(){
     YUI().use("node", function(YGet) {
         YGet.one("#divPrincipal").setStyle("backgroundImage", "url('"+BGImagePath+BGImage3+"')");
-		YGet.one("#divPrincipal").setStyle("background-repeat", "no-repeat");
-		YGet.one("#divPrincipal").setStyle("background-attachment", "fixed");
-		YGet.one("#divPrincipal").setStyle("height", "500px");
-		YGet.one("#divPrincipal").setStyle("width", "600px");
-		YGet.one("#divPrincipal").setStyle("top", "25px");		
-    });
-}
+	YGet.one("#divPrincipal").setStyle("background-repeat", "no-repeat");
+	YGet.one("#divPrincipal").setStyle("background-attachment", "fixed");
+	YGet.one("#divPrincipal").setStyle("height", "500px");
+	YGet.one("#divPrincipal").setStyle("width", "600px");
+	YGet.one("#divPrincipal").setStyle("top", "25px");		
+    });	
+})();
+
+
+
+
